6d777044762bba8e85af7e03f392feee8ea3a87e
Simone27 <luigimario.sonic@gmail.com>
2016-11-03 12:56:20 +0100

    Add files via upload

--- pag69es24.cpp
+++ pag69es24.cpp
@@ -6,12 +6,14 @@ int main (){
     int N=0;
     int n=0;
     int mas=0;
-    int mi_n;
+    int mino;
+    int pos_ma=0;
+    int pos_mi=0;
     cout << "Inserisci quanti numeri ci sono nella sequenza: ";
     cin >> N;
     cout << "Inserisci i numeri (>0): "<< endl;
     for (int i=0;i<N;i++) {
-        cin >> n;
+        cin n;
         do {
             if (n<=0) {
                 cout<< "IL NUMERO DEVE ESSERE MAGGIORE DI 0!"<<endl;
@@ -20,11 +22,13 @@ int main (){
         } while (n<=0);
         if (n>mas) {
             mas=n;
+            pos_ma=i+1;
         }
-        if (n<mi_n) {
-            mi_n=n;
+        if (n<mino) {
+            mino=n;
+            pos_mi=i+1;
         }
     }
-    cout<< "Il valore massimo e': "<<mas<<endl<<"Il valore minimo e': "<<mi_n<<endl;
+    cout<< "Il valore massimo e': "<<mas<<"  ed e' in posizione: "<<pos_ma<<endl<<"Il valore minimo e': "<<mino<<" ed e' in posizione: "<<pos_mi<<endl;
     return 0;
 }
