454d55c4f3c17a531fb53abd541873d1f2d047fc
Simone27 <luigimario.sonic@gmail.com>
2016-11-03 13:01:59 +0100

    Update pag69es24.cpp

--- pag69es24.cpp
+++ pag69es24.cpp
@@ -9,11 +9,15 @@ int main (){
     int mino;
     int pos_ma=0;
     int pos_mi=0;
+    bool primo_c=true;
     cout << "Inserisci quanti numeri ci sono nella sequenza: ";
     cin >> N;
     cout << "Inserisci i numeri (>0): "<< endl;
     for (int i=0;i<N;i++) {
         cin n;
+        if (primo_c == true) {
+            n=mino;
+        }
         do {
             if (n<=0) {
                 cout<< "IL NUMERO DEVE ESSERE MAGGIORE DI 0!"<<endl;
@@ -28,6 +32,7 @@ int main (){
             mino=n;
             pos_mi=i+1;
         }
+        primo_c=false;
     }
     cout<< "Il valore massimo e': "<<mas<<"  ed e' in posizione: "<<pos_ma<<endl<<"Il valore minimo e': "<<mino<<" ed e' in posizione: "<<pos_mi<<endl;
     return 0;
