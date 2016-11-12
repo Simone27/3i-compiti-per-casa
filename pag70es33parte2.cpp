db1c6e25371e9788210a4e63bc1324f443505985
Simone27 <luigimario.sonic@gmail.com>
2016-11-12 18:28:10 +0100

    *INCOMPLETO* compiti per casa 14/11/16

---
+++ pag70es33parte2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    unsigned int x=0;
+    unsigned int y=0;
+    int ris=0;
+    cout<<"Inserisci la base: ";
+    cin>>x;
+    cout<<"Inserisci l'esponente: ";
+    cin>>y;
+    for (int i=0;i<=y;i++) {
+        ris=ris+x;
+    }
+    cout<<"Il risultato e': "<<ris<<endl;
+    return 0;
+}
